132ab7a271da222f4d42206d1e4b99d7a635415d
Fernando Staroski <fernandostaroski@gmail.com>
2018-06-23 10:01:31 -0300

    criado endpoint /clintes/{id}

---
+++ src/main/java/com/staroski/cursomc/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.staroski.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.staroski.cursomc.domain.Cliente;
+import com.staroski.cursomc.repositories.ClienteRepository;
+import com.staroski.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	public Cliente buscar(Integer id) {
+		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. ID:" + id + ", Tipo:" + Cliente.class.getName()));
+	}
+
+}
