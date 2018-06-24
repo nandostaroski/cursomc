6b4390ec998a4752e692ca03e5d13063cabc6c03
Fernando Staroski <fernandostaroski@gmail.com>
2018-06-24 13:28:09 -0300

    Paginacao com parametros opicionais

--- src/main/java/com/staroski/cursomc/services/CategoriaService.java
+++ src/main/java/com/staroski/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.staroski.cursomc.domain.Categoria;
@@ -44,5 +47,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		
 		return repository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String orderBy,String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		
+		return repository.findAll(pageRequest);
 	}
 }
