ac110a39c3146ac01019d3fc900eb6a1b6bc21da
Fernando Staroski <fernandostaroski@gmail.com>
2018-07-08 10:48:49 -0300

    adicionando validação por email customizada

--- src/main/java/com/staroski/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/staroski/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.staroski.cursomc.domain.Cliente;
 import com.staroski.cursomc.domain.enums.TipoCliente;
 import com.staroski.cursomc.dto.ClienteNewDTO;
+import com.staroski.cursomc.repositories.ClienteRepository;
 import com.staroski.cursomc.resources.exception.FieldMessage;
 import com.staroski.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,7 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repository.findByEmail(objDto.getEmail());
 		
+		if (aux != null) {
+			list.add(new FieldMessage("email","Email já existente"));
+		}
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
