98f192fca0f12c81455a7d9c40d06a8794f69c4d
Fernando Staroski <fernandostaroski@gmail.com>
2018-07-09 20:40:35 -0300

    inserindo Pedido via Post

--- src/main/java/com/staroski/cursomc/domain/Pagamento.java
+++ src/main/java/com/staroski/cursomc/domain/Pagamento.java
@@ -11,10 +11,12 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.staroski.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
-@Inheritance(strategy=InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 public abstract class Pagamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -25,7 +27,7 @@ public abstract class Pagamento implements Serializable {
 
 	@JsonIgnore
 	@OneToOne
-	@JoinColumn(name="pedido_id")
+	@JoinColumn(name = "pedido_id")
 	@MapsId
 	private Pedido pedido;
 
--- src/main/java/com/staroski/cursomc/services/PedidoService.java
+++ src/main/java/com/staroski/cursomc/services/PedidoService.java
@@ -1,9 +1,17 @@
 package com.staroski.cursomc.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.staroski.cursomc.domain.ItemPedido;
+import com.staroski.cursomc.domain.PagamentoComBoleto;
 import com.staroski.cursomc.domain.Pedido;
+import com.staroski.cursomc.domain.enums.EstadoPagamento;
+import com.staroski.cursomc.repositories.ItemPedidoRepository;
+import com.staroski.cursomc.repositories.PagamentoRepository;
 import com.staroski.cursomc.repositories.PedidoRepository;
 import com.staroski.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,9 +21,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repository;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado. ID:" + id + ", Tipo:" + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip :obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
