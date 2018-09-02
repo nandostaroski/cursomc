5ff0e0b6aa0c7d850ea5bb6dc03705c2b15c613d
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2018-09-02 19:32:04 -0300

    adicionando campo senha ao cliente, com criptografia ao campo

--- src/main/java/com/staroski/cursomc/config/SecurityConfig.java
+++ src/main/java/com/staroski/cursomc/config/SecurityConfig.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -53,5 +52,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/staroski/cursomc/domain/Cliente.java
+++ src/main/java/com/staroski/cursomc/domain/Cliente.java
@@ -1,149 +1,147 @@
 package com.staroski.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.staroski.cursomc.domain.enums.TipoCliente;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.staroski.cursomc.domain.enums.TipoCliente;
-
 @Entity
 public class Cliente implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	private String nome;
-	
-	@Column(unique=true)
-	private String email;
-	private String CpfOuCnpj;
-	private Integer tipo;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
 
-	@OneToMany(mappedBy = "cliente",cascade=CascadeType.ALL)
-	private List<Endereco> enderecos = new ArrayList<>();
+    @Column(unique = true)
+    private String email;
+    private String CpfOuCnpj;
+    private Integer tipo;
 
-	@ElementCollection
-	@CollectionTable(name = "TELEFONE")
-	private Set<String> telefones = new HashSet<>();
+    @JsonIgnore
+    private String senha;
 
-	@JsonIgnore
-	@OneToMany(mappedBy = "cliente")
-	private List<Pedido> pedidos = new ArrayList<>();
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Endereco> enderecos = new ArrayList<>();
 
-	public Cliente() {
+    @ElementCollection
+    @CollectionTable(name = "TELEFONE")
+    private Set<String> telefones = new HashSet<>();
 
-	}
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		CpfOuCnpj = cpfOuCnpj;
-		this.tipo = (tipoCliente == null) ? null : tipoCliente.getCod();
-	}
+    public Cliente() {
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente, String senha) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        CpfOuCnpj = cpfOuCnpj;
+        this.tipo = (tipoCliente == null) ? null : tipoCliente.getCod();
+        this.senha = senha;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getCpfOuCnpj() {
-		return CpfOuCnpj;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setCpfOuCnpj(String cpfOuCnpj) {
-		CpfOuCnpj = cpfOuCnpj;
-	}
+    public String getCpfOuCnpj() {
+        return CpfOuCnpj;
+    }
 
-	public TipoCliente getTipo() {
-		return TipoCliente.toEnum(tipo);
-	}
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        CpfOuCnpj = cpfOuCnpj;
+    }
 
-	public void setTipo(TipoCliente tipoCliente) {
-		this.tipo = tipoCliente.getCod();
-	}
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
 
-	public List<Endereco> getEnderecos() {
-		return enderecos;
-	}
+    public void setTipo(TipoCliente tipoCliente) {
+        this.tipo = tipoCliente.getCod();
+    }
 
-	public void setEnderecos(List<Endereco> enderecos) {
-		this.enderecos = enderecos;
-	}
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
 
-	public Set<String> getTelefones() {
-		return telefones;
-	}
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
 
-	public void setTelefones(Set<String> telefones) {
-		this.telefones = telefones;
-	}
+    public Set<String> getTelefones() {
+        return telefones;
+    }
 
-	public List<Pedido> getPedidos() {
-		return pedidos;
-	}
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
 
-	public void setPedidos(List<Pedido> pedidos) {
-		this.pedidos = pedidos;
-	}
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    public String getSenha() { return senha; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
